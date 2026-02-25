462b2f057af25202e77389c03bd59101d6bb49f5
Your Name <you@example.com>
2026-02-25 15:47:29 +0530

    new updateed code

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://qrapi.vocalheart.com/api",
   withCredentials: true, // 🔥 Cookie ke liye ZARURI hai
   headers: {
     "Content-Type": "application/json",
