704b84694c5b3c8045d5975ecd7176152b98d835
Your Name <you@example.com>
2026-03-06 10:44:44 +0530

    new updated code

--- app/lib/axios.js
+++ app/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.reviewbadhao.com/api",
-  withCredentials: true, // 🔥 Cookie ke liye ZARURI hai
+  withCredentials: true, //  Cookie ke liye ZARURI hai
   headers: {
     "Content-Type": "application/json",
   },
